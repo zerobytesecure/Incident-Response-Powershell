d8f10a6c93f07e6277e586b6a32d9fcf7bfc1445
root <root@ilak.threatdump.com>
2024-06-25 17:04:15 -0400

    Initial commit

---
+++ DFIR-Script.ps1
@@ -0,0 +1,278 @@
+$ASCIIBanner = @"
+  _____                                           _              _   _     _____    ______   _____   _____  
+ |  __ \                                         | |            | | | |   |  __ \  |  ____| |_   _| |  __ \ 
+ | |__) |   ___   __      __   ___   _ __   ___  | |__     ___  | | | |   | |  | | | |__      | |   | |__) |
+ |  ___/   / _ \  \ \ /\ / /  / _ \ | '__| / __| | '_ \   / _ \ | | | |   | |  | | |  __|     | |   |  _  / 
+ | |      | (_) |  \ V  V /  |  __/ | |    \__ \ | | | | |  __/ | | | |   | |__| | | |       _| |_  | | \ \ 
+ |_|       \___/    \_/\_/    \___| |_|    |___/ |_| |_|  \___| |_| |_|   |_____/  |_|      |_____| |_|  \_\
+"@
+Write-Host $ASCIIBanner
+Write-Host "`n"
+Write-Host "By twitter: @BertJanCyber, Github: Bert-JanP"
+Write-Host "===========================================`n"
+
+$IsAdmin = ([Security.Principal.WindowsPrincipal] `
+        [Security.Principal.WindowsIdentity]::GetCurrent() `
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if ($IsAdmin) {
+    Write-Host "DFIR Session starting as Administrator..."
+}
+else {
+    Write-Host "No Administrator session detected. For the best performance run as Administrator. Not all items can be collected..."
+    Write-Host "DFIR Session starting..."
+}
+
+Write-Host "Creating output directory..."
+$CurrentPath = Get-Location
+$ExecutionTime = $(get-date -f yyyy-MM-dd)
+$FolderCreation = "$CurrentPath\DFIR-$env:computername-$ExecutionTime"
+mkdir -Force $FolderCreation | Out-Null
+Write-Host "Output directory created: $FolderCreation..."
+
+function Get-IPInfo {
+    Write-Host "Collecting local ip info..."
+    $Ipinfoutput = "$FolderCreation\ipinfo.txt"
+    Get-NetIPAddress | Out-File -Force -FilePath $Ipinfoutput
+}
+function Get-ShadowCopies {
+    Write-Host "Collecting Shadow Copies..."
+    $ShadowCopy = "$FolderCreation\ShadowCopies.txt"
+    Get-CimInstance Win32_ShadowCopy | Out-File -Force -FilePath $ShadowCopy
+}
+
+function Get-OpenConnections {
+    Write-Host "Collecting Open Connections..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    mkdir -Force $ConnectionFolder | Out-Null
+    $Ipinfoutput = "$ConnectionFolder\OpenConnections.txt"
+    Get-NetTCPConnection -State Established | Out-File -Force -FilePath $Ipinfoutput
+}
+
+function Get-AutoRunInfo {
+    Write-Host "Collecting AutoRun info..."
+    $AutoRunFolder = "$FolderCreation\Persistence"
+    mkdir -Force $AutoRunFolder | Out-Null
+    $RegKeyOutput = "$AutoRunFolder\AutoRunInfo.txt"
+    Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User | Format-List | Out-File -Force -FilePath $RegKeyOutput
+}
+
+function Get-InstalledDrivers {
+    Write-Host "Collecting Installed Drivers..."
+    $AutoRunFolder = "$FolderCreation\Persistence"
+    $RegKeyOutput = "$AutoRunFolder\InstalledDrivers.txt"
+    driverquery | Out-File -Force -FilePath $RegKeyOutput
+}
+
+function Get-ActiveUsers {
+    Write-Host "Collecting Active users..."
+    $UserFolder = "$FolderCreation\UserInformation"
+    mkdir -Force $UserFolder | Out-Null
+    $ActiveUserOutput = "$UserFolder\ActiveUsers.txt"
+    query user /server:$server | Out-File -Force -FilePath $ActiveUserOutput
+}
+
+function Get-LocalUsers {
+    Write-Host "Collecting Local users..."
+    $UserFolder = "$FolderCreation\UserInformation"
+    $ActiveUserOutput = "$UserFolder\LocalUsers.txt"
+    Get-LocalUser | Format-Table | Out-File -Force -FilePath $ActiveUserOutput
+}
+
+function Get-ActiveProcesses {
+    Write-Host "Collecting Active Processes..."
+    $ProcessFolder = "$FolderCreation\ProcessInformation"
+    New-Item -Path $ProcessFolder -ItemType Directory -Force | Out-Null
+    $UniqueProcessHashOutput = "$ProcessFolder\UniqueProcessHash.csv"
+    $ProcessListOutput = "$ProcessFolder\ProcessList.csv"
+
+    $processes_list = @()
+    foreach ($process in (Get-WmiObject Win32_Process | Select-Object Name, ExecutablePath, CommandLine, ParentProcessId, ProcessId))
+    {
+        $process_obj = New-Object PSCustomObject
+        if ($process.ExecutablePath -ne $null)
+        {
+            $hash = (Get-FileHash -Algorithm SHA256 -Path $process.ExecutablePath).Hash 
+            $process_obj | Add-Member -NotePropertyName Proc_Hash -NotePropertyValue $hash
+            $process_obj | Add-Member -NotePropertyName Proc_Name -NotePropertyValue $process.Name
+            $process_obj | Add-Member -NotePropertyName Proc_Path -NotePropertyValue $process.ExecutablePath
+            $process_obj | Add-Member -NotePropertyName Proc_CommandLine -NotePropertyValue $process.CommandLine
+            $process_obj | Add-Member -NotePropertyName Proc_ParentProcessId -NotePropertyValue $process.ParentProcessId
+            $process_obj | Add-Member -NotePropertyName Proc_ProcessId -NotePropertyValue $process.ProcessId
+            $processes_list += $process_obj
+        }   
+    }
+
+    ($processes_list | Select-Object Proc_Path, Proc_Hash -Unique).GetEnumerator() | Export-Csv -NoTypeInformation -Path $UniqueProcessHashOutput
+    ($processes_list | Select-Object Proc_Name, Proc_Path, Proc_CommandLine, Proc_ParentProcessId, Proc_ProcessId, Proc_Hash).GetEnumerator() | Export-Csv -NoTypeInformation -Path $ProcessListOutput
+}
+
+function Get-SecurityEventCount {
+    Write-Host "Collecting stats Security Events last 48 hours..."
+    $SecurityEvents = "$FolderCreation\SecurityEvents"
+    mkdir -Force $SecurityEvents | Out-Null
+    $ProcessOutput = "$SecurityEvents\EventCount.txt"
+    $SecurityEvents = Get-EventLog -LogName security -After (Get-Date).AddDays(-2)
+    $SecurityEvents | Group-Object -Property EventID -NoElement | Sort-Object -Property Count -Descending | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-SecurityEvents {
+    Write-Host "Collecting Security Events last 48 hours..."
+    $SecurityEvents = "$FolderCreation\SecurityEvents"
+    mkdir -Force $SecurityEvents | Out-Null
+    $ProcessOutput = "$SecurityEvents\SecurityEvents.txt"
+    get-eventlog security -After (Get-Date).AddDays(-2) | Format-List * | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-EVTXFiles {
+    Write-Host "Collecting Important EVTX Files..."
+    $EventViewer = "$FolderCreation\Event Viewer"
+    mkdir -Force $EventViewer | Out-Null
+    $evtxPath = "C:\Windows\System32\winevt\Logs"
+    $channels = @(
+        "Application",
+        "Security",
+        "System",
+        "Microsoft-Windows-Sysmon%4Operational",
+        "Microsoft-Windows-TaskScheduler%4Operational",
+        "Microsoft-Windows-PowerShell%4Operational"
+    )
+
+    foreach ($channel in $channels) {
+        Copy-Item -Path "$($EventViewer)\$($channel).evtx" -Destination "$($EventViewer)\$($channel).evtx"
+    }
+}
+
+function Get-OfficeConnections {
+    Write-Host "Collecting connections made from office applciations..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    $OfficeConnection = "$ConnectionFolder\ConnectionsMadeByOffice.txt"
+    Get-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Internet\Server Cache* -erroraction 'silentlycontinue' | Out-File -Force -FilePath $OfficeConnection 
+}
+
+function Get-NetworkShares {
+    Write-Host "Collecting Active Network Shares..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    $ProcessOutput = "$ConnectionFolder\NetworkShares.txt"
+    Get-ChildItem -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\ | Format-Table | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-SMBShares {
+    Write-Host "Collecting SMB Shares..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    $ProcessOutput = "$ConnectionFolder\SMBShares.txt"
+    Get-SmbShare | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-RDPSessions {
+    Write-Host "Collecting RDS Sessions..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    $ProcessOutput = "$ConnectionFolder\RDPSessions.txt"
+    qwinsta /server:localhost | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-RemotelyOpenedFiles {
+    Write-Host "Collecting Remotly Opened Files..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    $ProcessOutput = "$ConnectionFolder\RemotelyOpenedFiles.txt"
+    openfiles | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-DNSCache {
+    Write-Host "Collecting DNS Cache..."
+    $ConnectionFolder = "$FolderCreation\Connections"
+    $ProcessOutput = "$ConnectionFolder\DNSCache.txt"
+    Get-DnsClientCache | Format-List | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-PowershellHistory {
+    Write-Host "Collecting Powershell History..."
+    $PowershellHistoryOutput = "$FolderCreation\PowershellHistory.txt"
+    history | Out-File -Force -FilePath $PowershellHistoryOutput
+}
+
+function Get-RecentlyInstalledSoftwareEventLogs {
+    Write-Host "Collecting Recently Installed Software EventLogs..."
+    $ApplicationFolder = "$FolderCreation\Applications"
+    mkdir -Force $ApplicationFolder | Out-Null
+    $ProcessOutput = "$ApplicationFolder\RecentlyInstalledSoftwareEventLogs.txt"
+    Get-WinEvent -ProviderName msiinstaller | where id -eq 1033 | select timecreated,message | FL *| Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-RunningServices {
+    Write-Host "Collecting Running Services..."
+    $ApplicationFolder = "$FolderCreation\Applications"
+    $ProcessOutput = "$ApplicationFolder\RecentlyInstalledSoftwareEventLogs.txt"
+    Get-Service | Where-Object {$_.Status -eq "Running"} | format-list | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-ScheduledTasks {
+    Write-Host "Collecting Scheduled Tasks..."
+    $ScheduledTaskFolder = "$FolderCreation\ScheduledTask"
+    mkdir -Force $ScheduledTaskFolder| Out-Null
+    $ProcessOutput = "$ScheduledTaskFolder\ScheduledTasksList.txt"
+    Get-ScheduledTask | Where-Object {$_.State -ne "Disabled"} | Format-List | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-ScheduledTasksRunInfo {
+    Write-Host "Collecting Scheduled Tasks Run Info..."
+    $ScheduledTaskFolder = "$FolderCreation\ScheduledTask"
+    $ProcessOutput = "$ScheduledTaskFolder\ScheduledTasksListRunInfo.txt"
+    Get-ScheduledTask | Where-Object {$_.State -ne "Disabled"} | Get-ScheduledTaskInfo | Out-File -Force -FilePath $ProcessOutput
+}
+
+function Get-ConnectedDevices {
+    Write-Host "Collecting Information about Connected Devices..."
+    $DeviceFolder = "$FolderCreation\ConnectedDevices"
+    New-Item -Path $DeviceFolder -ItemType Directory -Force | Out-Null
+    $ConnectedDevicesOutput = "$DeviceFolder\ConnectedDevices.csv"
+
+    Get-PnpDevice | Export-Csv -NoTypeInformation -Path $ConnectedDevicesOutput
+}
+
+
+function Zip-Results {
+    Write-Host "Write results to $FolderCreation.zip..."
+    Compress-Archive -Force -LiteralPath $FolderCreation -DestinationPath "$FolderCreation.zip"
+}
+
+#Run all functions that do not require admin priviliges
+function Run-WithoutAdminPrivilege {
+    Get-IPInfo
+    Get-OpenConnections
+    Get-AutoRunInfo
+    Get-ActiveUsers
+    Get-LocalUsers
+    Get-ActiveProcesses
+    Get-OfficeConnections
+    Get-NetworkShares
+    Get-SMBShares
+    Get-RDPSessions
+    Get-PowershellHistory
+    Get-DNSCache
+    Get-InstalledDrivers    
+    Get-RecentlyInstalledSoftwareEventLogs
+    Get-RunningServices
+    Get-ScheduledTasks
+    Get-ScheduledTasksRunInfo
+    Get-ConnectedDevices
+}
+
+#Run all functions that do require admin priviliges
+Function Run-WithAdminPrivilges {
+    Get-SecurityEventCount
+    Get-SecurityEvents
+    Get-RemotelyOpenedFiles
+    Get-ShadowCopies
+    Get-EVTXFiles
+}
+
+if ($IsAdmin) {
+    Run-WithoutAdminPrivilege
+    Run-WithAdminPrivilges
+}
+else {
+    Run-WithoutAdminPrivilege
+}
+
+Zip-Results
---
+++ README.md
@@ -0,0 +1,91 @@
+# Powershell Digital Forensics & Incident Response (DFIR)[![Tweet](https://img.shields.io/twitter/url/http/shields.io.svg?style=social)](https://twitter.com/intent/tweet?text=Powershell%20DFIR!&url=https://github.com/Bert-JanP/Incident-Response-Powershell)
+
+This repository contains multiple PowerShell scripts that can help you respond to cyber attacks on Windows Devices.
+
+The following Incident Response scripts are included:
+- [DFIR Script](./DFIR-Script.ps1): Collects all items as listed in section [DFIR Script](#dfir-script).
+- [CollectWindowsEvents](./Scripts/CollectWindowsEvents.ps1): Collects all Windows events and outputs it as CSV.
+- [CollectWindowsSecurityEvents](./Scripts/CollectWindowsSecurityEvents.ps1): Collects all Windows security events and outputs it as CSV.
+- [CollectPnPDevices](./Scripts/CollectPnPDevices.ps1): Collects all Plug and Play devices, such as USB, Network and Storage.
+- [DumpLocalAdmins](./Scripts/DumpLocalAdmins.ps1): Returns all local admins of a device.
+- [LastLogons](./Scripts/LastLogons.ps1) - List the last N successful logins of a device.
+- [ListInstalledSecurityProducts](./Scripts/ListInstalledSecurityProducts.ps1) - List the installed security products and their status.
+
+## DFIR Script
+The [DFIR script](./DFIR-Script.ps1) collects information from multiple sources and structures the output in the current directory in a folder named 'DFIR-_hostname_-_year_-_month_-_date_'. This folder is zipped at the end, so that folder can be remotely collected. This script can also be used within Defender For Endpoint in a Live Response session (see below). The DFIR script collects the following information when running as normal user:
+- Local IP Info
+- Open Connections
+- Aautorun Information (Startup Folder & Registry Run keys)
+- Active Users
+- Local Users
+- Connections Made From Office Applications
+- Active SMB Shares
+- RDP Sessions
+- Active Processes
+- Active USB Connections
+- Powershell History
+- DNS Cache
+- Installed Drivers
+- Installed Software
+- Running Services
+- Scheduled Tasks
+
+For the best experience run the script as admin, then the following items will also be collected:
+- Windows Security Events
+- Remotely Opened Files
+- Shadow Copies
+
+## DFIR Commands
+The [DFIR Commands page](./DFIR-Commands.md) contains invidividual powershell commands that can be used during your incident response process. The follwing catagories are defined:
+- Connections
+- Persistence
+- Windows Security Events
+- Processes
+- User & Group Information
+- Applications
+- File Analysis
+- Collect IOC Information
+
+## Windows Usage
+
+The script can be excuted by running the following command.
+```PowerShell
+.\DFIR-Script.ps1
+```
+
+The script is unsigned, that could result in having to use the -ExecutionPolicy Bypass to run the script.
+```PowerShell
+Powershell.exe -ExecutionPolicy Bypass .\DFIR-Script.ps1
+```
+
+## DFIR Script | Defender For Endpoit Live Response Integration
+It is possible to use the DFIR Script in combination with the Defender For Endpoint Live Repsonse. Make sure that Live Response is setup  (See DOCS). Since my script is usigned a setting change must be made to able to run the script.
+
+There is a blog article available that explains more about how to leverage Custom Script in Live Response: [Incident Response Part 3: Leveraging Live Response](https://kqlquery.com/posts/leveraging-live-response/)
+
+To run unsigned scripts live Response:
+- Security.microsoft.com
+- Settings
+- Endpoints
+- Advanced Features
+- Make sure that Live Response is enabled
+- If you want to run this on a server enable live resonse for servers
+- Enable Live Response unsigened script execution
+
+Execute script:
+- Go to the device page
+- Initiate Live Response session
+- Upload File to library to upload script
+- After uploading the script to the library, use the ***run*** command to run the script
+
+To collect the output of the DFIR script perform the following actions:
+```PowerShell
+getfile "C:\windows\DFIR-TestDevice-2022-07-06.zip" &	
+```
+
+### Docs
+- [Microsoft Documentation Live Response](https://docs.microsoft.com/en-us/microsoft-365/security/defender-endpoint/live-response?view=o365-worldwide)
+- [DFE User permissions](https://docs.microsoft.com/en-us/microsoft-365/security/defender-endpoint/user-roles?view=o365-worldwide)
+- [Defender For Endpoint Settings Live Response](https://docs.microsoft.com/en-us/microsoft-365/security/defender-endpoint/advanced-features?view=o365-worldwide#live-response)
+
+
